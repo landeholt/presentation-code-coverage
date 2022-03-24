5dfa18ad2cb58f85aabcc0ec69c37a15a5c60aa3
landeholt <johnlan@kth.se>
2022-03-24 16:16:31 +0100

    add test cases

--- tests/test_module_a.py
+++ tests/test_module_a.py
@@ -3,7 +3,7 @@ import pytest
 from project.module_a.core import addition, subtraction
 
 def test_addition():
-    assert addition(60,9) == 60
+    assert addition(60,9) == 69
 
 def test_subtraction():
-    assert subtraction(42069, 42000) == 60
+    assert subtraction(42069, 42000) == 69
